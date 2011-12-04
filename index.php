767e571e5c5b5767c7d311242d67f5c1fadb2987
LordLobo <dan.giralte@gmail.com>
2011-12-03 18:19:50 -0600

    add index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: lordlobo
+ * Date: 12/3/11
+ * Time: 6:17 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+echo "Hello World";
+
+?>
