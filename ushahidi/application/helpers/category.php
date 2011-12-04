21635346327c922e67590b52d9fcdddc0d2025fc
Aki Braun <aki.kyozoku@gmail.com>
2011-12-03 21:55:08 -0600

    bugfix for table prefix space problem

--- ushahidi/application/helpers/category.php
+++ ushahidi/application/helpers/category.php
@@ -9,9 +9,9 @@
  */
 class category_Core {
 
-		/**
-		 * Displays a single category checkbox.
-		 */
+	/**
+	 * Displays a single category checkbox.
+	 */
 	public static function display_category_checkbox($category, $selected_categories, $form_field, $enable_parents = FALSE)
 	{
 		$html = '';
@@ -21,9 +21,7 @@ class category_Core {
 		// Get locale
 		$l = Kohana::config('locale.language.0');
 
-		$translated_title = Category_Lang_Model::category_title($cid, $l);
-		
-		$category_title = ($translated_title)? $translated_title :  $category->category_title;
+		$category_title = Category_Lang_Model::category_title($cid, $l);
 
 		//$category_title = $category->category_title;
 		$category_color = $category->category_color;
@@ -31,10 +29,22 @@ class category_Core {
 		// Category is selected.
 		$category_checked = in_array($cid, $selected_categories);
 
-		$disabled = "";
-		if (!$enable_parents AND $category->children->count() > 0)
+		// Visible Child Count
+		$vis_child_count = 0;
+		foreach ($category->children as $child)
 		{
-			$disabled = " disabled=\"disabled\"";
+			$child_visible = $child->category_visible;
+			if ($child_visible)
+			{
+				// Increment Visible Child count
+				++$vis_child_count;
+			}
+		}
+
+		$disabled = "";
+		if (!$enable_parents AND $category->children->count() > 0 AND $vis_child_count >0)
+		{
+			$disabled = " disabled=\"disabled\"";	
 		}
 
 		$html .= form::checkbox($form_field.'[]', $cid, $category_checked, ' class="check-box"'.$disabled);
@@ -77,14 +87,29 @@ class category_Core {
 			$html .= '<li>';
 			$html .= category::display_category_checkbox($category, $selected_categories, $form_field, $enable_parents);
 
+			// Visible Child Count
+			$vis_child_count = 0;
+			foreach ($category->children as $child)
+			{
+				$child_visible = $child->category_visible;
+				if ($child_visible)
+				{
+					// Increment Visible Child count
+					++$vis_child_count;
+				}
+			}
 			// Display child categories.
-			if ($category->children->count() > 0)
+			if ($category->children->count() > 0 AND $vis_child_count > 0)
 			{
 				$html .= '<ul>';
 				foreach ($category->children as $child)
 				{
-					$html .= '<li>';
-					$html .= category::display_category_checkbox($child, $selected_categories, $form_field, $enable_parents);
+					$child_visible = $child->category_visible;
+					if ($child_visible)
+					{
+						$html .= '<li>';
+						$html .= category::display_category_checkbox($child, $selected_categories, $form_field, $enable_parents);
+					}
 				}
 				$html .= '</ul>';
 			}
@@ -101,7 +126,7 @@ class category_Core {
 
 		return $html;
 	}
-	
+
 	/**
 	 * Generates a category tree view - recursively iterates
 	 *
@@ -111,25 +136,25 @@ class category_Core {
 	{
 		// To hold the category data
 		$category_data = array();
-		
+
 		// Database table prefix
 		$table_prefix = Kohana::config('database.default.table_prefix');
-		
+
 		// Database instance
 		$db = new Database();
-		
+
 		// Fetch all the top level parent categories
 		foreach (Category_Model::get_categories() as $category)
 		{
 			self::_extend_category_data($category_data, $category);
 		}
-		
+
 		// NOTES: Emmanuel Kala - Aug 5, 2011
 		// Initialize the report totals for the parent categories just in case
 		// the deployment does not have sub-categories in which case the query
 		// below won't return a result
 		self::_init_parent_category_report_totals($category_data, $table_prefix);
-		
+
 		// Query to fetch the report totals for the parent categories
 		$sql = "SELECT c2.id,  COUNT(DISTINCT ic.incident_id)  AS report_count "
 			. "FROM ".$table_prefix."category c, ".$table_prefix."category c2, ".$table_prefix."incident_category ic "
@@ -143,7 +168,7 @@ class category_Core {
 			. "AND c2.category_title != \"Trusted Reports\" "
 			. "GROUP BY c2.id "
 			. "ORDER BY c2.id ASC";
-		
+
 		// Update the report_count field of each top-level category
 		foreach ($db->query($sql) as $category_total)
 		{
@@ -154,7 +179,7 @@ class category_Core {
 				$category_data[$category_total->id]['report_count'] = $category_total->report_count;
 			}
 		}
-		
+
 		// Fetch the other categories
 		$sql = "SELECT c.id, c.parent_id, c.category_title, c.category_color, COUNT(c.id) report_count "
 			. "FROM ".$table_prefix."category c "
@@ -164,18 +189,18 @@ class category_Core {
 			. "AND i.incident_active = 1 "
 			. "GROUP BY c.category_title "
 			. "ORDER BY c.category_title ASC";
-		
+
 		// Add child categories
 		foreach ($db->query($sql) as $category)
 		{
 			// Extend the category data array
 			self::_extend_category_data($category_data, $category);
-			
+
 			if (array_key_exists($category->parent_id, $category_data))
 			{
 				// Add children
 				$category_data[$category->parent_id]['children'][$category->id] = array(
-					'category_title' => $category->category_title,
+					'category_title' => Category_Lang_Model::category_title($category->id,Kohana::config('locale.language.0')),
 					'parent_id' => $category->parent_id,
 					'category_color' => $category->category_color,
 					'report_count' => $category->report_count,
@@ -183,11 +208,11 @@ class category_Core {
 				);
 			}
 		}
-		
+
 		// Generate and return the HTML
 		return self::_generate_treeview_html($category_data);
 	}
-	
+
 	/**
 	 * Helper method for adding parent categories to the category data
 	 *
@@ -207,9 +232,9 @@ class category_Core {
 		{
 			// Get the report count
 			$report_count = property_exists($temp_category, 'report_count')? $temp_category->report_count : 0;
-			
+
 			$array[$temp_category->id] = array(
-				'category_title' => $temp_category->category_title,
+				'category_title' => Category_Lang_Model::category_title($temp_category->id,Kohana::config('locale.language.0')),
 				'parent_id' => $temp_category->parent_id,
 				'category_color' => $temp_category->category_color,
 				'report_count' => $report_count,
@@ -221,7 +246,7 @@ class category_Core {
 
 		return FALSE;
 	}
-	
+
 	/**
 	 * Traverses an array containing category data and returns a tree view
 	 *
@@ -232,26 +257,26 @@ class category_Core {
 	{
 		// To hold the treeview HTMl
 		$tree_html = "";
-		
+
 		foreach ($category_data as $id => $category)
 		{
 			// Determine the category class
 			$category_class = ($category['parent_id'] > 0)? " class=\"report-listing-category-child\"" : "";
-			
+
 			$tree_html .= "<li".$category_class.">"
 							. "<a href=\"#\" class=\"cat_selected\" id=\"filter_link_cat_".$id."\">"
 							. "<span class=\"item-swatch\" style=\"background-color: #".$category['category_color']."\">&nbsp;</span>"
 							. "<span class=\"item-title\">".strip_tags($category['category_title'])."</span>"
 							. "<span class=\"item-count\">".$category['report_count']."</span>"
 							. "</a></li>";
-							
+
 			$tree_html .= self::_generate_treeview_html($category['children']);
 		}
-		
+
 		// Return
 		return $tree_html;
 	}
-	
+
 	/**
 	 * Initializes the report totals for the parent categories
 	 *
@@ -263,16 +288,16 @@ class category_Core {
 		// Query to fetch the report totals for the parent categories
 		$sql = "SELECT c.id, COUNT(DISTINCT ic.incident_id) AS report_count "
 			. "FROM ".$table_prefix."category c "
-			. "INNER JOIN ".$table_prefix." incident_category ic ON (ic.category_id = c.id) "
-			. "INNER JOIN ".$table_prefix." incident i ON (ic.incident_id = i.id) "
+			. "INNER JOIN ".$table_prefix."incident_category ic ON (ic.category_id = c.id) "
+			. "INNER JOIN ".$table_prefix."incident i ON (ic.incident_id = i.id) "
 			. "WHERE c.category_visible = 1 "
 			. "AND i.incident_active = 1 "
 			. "AND c.parent_id = 0 "
 			. "GROUP BY c.id";
-		
+
 		// Fetch the records
 		$result = Database::instance()->query($sql);
-		
+
 		// Set the report totals for each of the parent categorie
 		foreach ($result as $category)
 		{
@@ -281,7 +306,7 @@ class category_Core {
 				$category_data[$category->id]['report_count'] = $category->report_count;
 			}
 		}
-		
+
 		// Garbage collection
 		unset ($sql, $result);
 	}
