0034dcbec61795a88d6a4d96cc81ecfbf2769060
LordLobo <dan.giralte@gmail.com>
2011-12-04 00:13:30 -0600

    new directories

---
+++ ajax/main.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: lordlobo
+ * Date: 12/4/11
+ * Time: 12:09 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+// Global Functions
+
---
+++ ajax/report.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: lordlobo
+ * Date: 12/4/11
+ * Time: 12:05 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+require_once(APPPATH.'views/map_common_js.php');
+
